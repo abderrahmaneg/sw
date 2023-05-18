687126e3d90563aca6febeb33aa9aa3e717bbe31
abderrahmaneg <105305990+abderrahmaneg@users.noreply.github.com>
2023-05-18 09:23:03 +0100

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,24 +1,29 @@
-import React from 'react';
+import React, { useState } from 'react';
 import Login from './components/Login';
 import Forgotpassword from './components/FirstPage';
 import SecondPage from './components/SecondPage';
 import ThirdPage from './components/ThirdPage';
 import LastPage from './components/LastPage';
-
+import Dashboard from './components/Dashboard';
 import './App.css';
 import { Routes, Route, BrowserRouter } from 'react-router-dom';
 
 function App() {
+  
+  const [loggedIn,setLoggedIn]=useState(false);
+  const [role,setRole]=useState('');
   return (
     <div className='container' >
       <BrowserRouter>
 
         <Routes>
-          <Route path='/' exact element={<Login />}></Route>
+          <Route path='/login' exact element={<Login setLoggedIn={setLoggedIn} setRole={setRole} />}></Route>
           <Route path='/forgotpassword' exact element={<Forgotpassword />} ></Route>
           <Route path="/send-link"  exact element={<SecondPage />} ></Route>
           <Route path="/reset-password"  exact element={<ThirdPage />} ></Route>
           <Route path="/success"  exact element={<LastPage />} ></Route>
+          <Route path="/Dashboard"  exact element={<Dashboard loggedIn={loggedIn} role={role} />} ></Route>
+
         </Routes>
 
 
